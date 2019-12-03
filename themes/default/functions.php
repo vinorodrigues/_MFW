c99f16f115aa56d468a8f6d019cabfd40f90f600
Vino Rodrigues <github@m.vinorodrigues.com>
2019-12-03 23:15:21 +1100

    .

--- themes/default/functions.php
+++ themes/default/functions.php
@@ -1,11 +1,11 @@
 <?php
 
-add_style_sheet('bootstrap', 'vendor/bootstrap/css/bootstrap.css');
+add_style_sheet('bootstrap', 'vendor/bootswatch/flatly/bootstrap.min.css');
 add_style_sheet('fa', 'vendor/fontawesome/css/fontawesome.css');
 add_style_sheet('fas', 'vendor/fontawesome/css/solid.css');
+add_style_sheet('bootstrap-dark', 'vendor/bootswatch/darkly/bootstrap.min.css', 10, '(prefers-color-scheme: dark)');
+add_style_sheet('bootstrap-light', 'vendor/bootswatch/flatly/bootstrap.min.css', 10, '(prefers-color-scheme: light)');
 
 add_script_file('jquery', 'vendor/jquery/js/jquery-3.4.1.js');
 add_script_file('popper', 'vendor/popper/js/popper.js');
 add_script_file('bootstrap', 'vendor/bootstrap/js/bootstrap.js');
-
-include_once 'fn-darkmode.php';
